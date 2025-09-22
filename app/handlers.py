7ec4c428c39e2ec55470cd9a07d681f5e12584ab
DevOnaComunicacao <desenvolvedor@onacomunicacao.com.br>
2025-09-22 16:00:10 -0300

    adicionadas jsonResponses do fastapi nos handlers

--- app/handlers.py
+++ app/handlers.py
@@ -5,17 +5,17 @@ from app.send import enviar_lidermedtech, enviar_lidermed
 def handler_lidermedtech(interessados):
 
     if interessados.nome == "":
-        return {"erro": "insira um nome"}
+        return JSONResponse(content={"erro": "insira um nome"})
    # if interessados.empresa == "":
-   #     return {"erro": "insira uma empresa"}
+        return {"erro": "insira uma empresa"}
   #  if interessados.cnpj == "":
         return {"erro": "insira um cnpj válido"}
   #  if interessados.cargo == "":
         return {"erro": "insira um cargo"}
     if interessados.email == "":
-        return {"erro": "insira um email válido"}
+        return JSONResponse(content={"erro": "insira um email válido"})
     if interessados.whatsapp == "":
-        return {"erro": "insira um whatsapp válido"}
+        return JSONResponse(content={"erro": "insira um whatsapp válido"})
   #  if interessados.demonstracao != "sim" and interessados.demonstracao != "não":
         return {"erro: campo inválido"}
   #  if interessados.utm_source == "":
@@ -28,22 +28,22 @@ def handler_lidermedtech(interessados):
 def handler_lidermed(compradores):
 
     if compradores.nome == "":
-        return {"erro": "insira um nome"}
+        return JSONResponse(content={"erro": "insira um nome"})
     if compradores.empresa == "":
-        return {"erro": "insira uma empresa"}
+        return JSONResponse(content={"erro": "insira uma empresa"})
     if compradores.cnpj == "":
-        return {"erro": "insira um cnpj válido"}
+        return JSONResponse(content={"erro": "insira um cnpj válido"})
     if compradores.cargo == "":
-        return {"erro": "insira um cargo"}
+        return JSONResponse(content={"erro": "insira um cargo"})
     if compradores.email == "":
-        return {"erro": "insira um email válido"}
+        return JSONResponse(content={"erro": "insira um email válido"})
     if compradores.whatsapp == "":
-        return {"erro": "insira um whatsapp válido"}
+        return JSONResponse(content={"erro": "insira um whatsapp válido"})
     if compradores.compra != "sim" and compradores.compra != "não":
-        return {"erro: campo inválido"}
+        return JSONResponse(content={"erro: campo inválido"})
     if compradores.utm_source == "":
-        return {"erro": "utm inválida"}
+        return JSONResponse(content={"erro": "utm inválida"})
     if compradores.data_hora == "":
-        return {"erro": "data inválida"}
+        return JSONResponse(content={"erro": "data inválida"})
 
     return enviar_lidermed(compradores)
