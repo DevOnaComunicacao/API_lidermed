d3b3badd854e483ba0d05697545382246a0c32fb
DevOnaComunicacao <desenvolvedor@onacomunicacao.com.br>
2025-10-01 15:33:49 -0300

    adicionado teste de formato json parte 12

--- app/main.py
+++ app/main.py
@@ -58,11 +58,11 @@ def post_lidermedtech(interessados: Interessados):
 async def post_lidermed(request: Request):
     form = await request.form()
     compradores = Compradores(
-        nome=form.get("form_fields[nome]"),
-        whatsapp=form.get("form_fields[whatsapp]"),
-        email=form.get("form_fields[email]"),
-        assunto=form.get("form_fields[assunto]"),
-        mensagem=form.get("form_fields[mensagem]")
+        nome=form.get("form_fields[nome]", ""),
+        whatsapp=form.get("form_fields[whatsapp]", ""),
+        email=form.get("form_fields[email]", ""),
+        assunto=form.get("form_fields[assunto]", ""),
+        mensagem=form.get("form_fields[mensagem]", "")
     )
     print(compradores.dict())
     return handler_lidermed(compradores)
